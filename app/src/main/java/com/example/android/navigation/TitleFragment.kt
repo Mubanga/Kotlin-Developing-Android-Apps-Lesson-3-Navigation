96e1984e573e00e42a7f7c5047cd2fcfed95f7d2
Mubanga <mubangeriseme@yahoo.com>
2019-07-14 23:41:06 +0200

    Lesson 3: Lesson 9: Navigation Pages Added (Let's Navigate Already)

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 
@@ -18,11 +19,12 @@ class TitleFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-            val _binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater,
-                    R.layout.fragment_title,container,false)
+        val _binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater,
+                R.layout.fragment_title, container, false)
+        // Use The Navigation Class To Create The OnClickListner Specified By The Action In " navigation.xml "
+        _binding.playButton.setOnClickListener { Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment) }
         return _binding.root
-        }
-
+    }
 
 
 }
